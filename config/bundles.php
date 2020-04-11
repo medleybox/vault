57fe9b2e8c5e702412d02acad4dfe8543e304de3
Chris Hilsdon <chrish@xigen.co.uk>
2020-04-11 23:03:34 +0100

    [composer] Required dunglas/doctrine-json-odm for JSON data types in doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Dunglas\DoctrineJsonOdm\Bundle\DunglasDoctrineJsonOdmBundle::class => ['all' => true],
 ];
