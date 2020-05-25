8e63e2665a2fb1658d5aa265ebdfbb656b5a58ad
Chris Hilsdon <chrish@xigen.co.uk>
2020-05-25 20:22:34 +0100

    Implemented plugin system

---
+++ src/Plugins/AbstractPlugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MedleyBox\Plugins;
+
+use Symfony\Component\HttpKernel\Bundle\{Bundle, BundleInterface};
+
+/**
+ * Base class for Symfony bundles written for Medleybox
+ */
+class AbstractPlugin extends Bundle implements BundleInterface
+{
+}
